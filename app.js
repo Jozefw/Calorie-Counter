a5a20bca923288803eee49d6ce32367a9737adee
Jozef <jozefw@sbcglobal.net>
2018-02-04 20:15:24 -0800

    add UI Controller

--- app.js
+++ app.js
@@ -1,58 +1,73 @@
-// Storage Controller
+// ************* Local Storage Controller ***********
 
 
-// Item Controller
-const ItemCtrl = (function(){
-  // Item Constructor
-  const Item = function(id, name, calories){
+// *************** Item  Controller ******************
+const itemCtrl = (function(){
+  // item constructor
+  const Item = function(id,name,calories){
     this.id = id;
     this.name = name;
     this.calories = calories;
   }
+// Data structure or state
+const data = {
+  items:[
+    {id:0,name:'Apple Pie',calories:1000},
+    {id:1,name:'Chocolate Cream Pie',calories:2000},
+    {id:2,name:'Tofu Pie',calories:10}
 
-  // Data Structure / State
-  const data = {
-    items: [
-      {id: 0, name: 'Steak Dinner', calories: 1200},
-      {id: 1, name: 'Cookie', calories: 400},
-      {id: 2, name: 'Eggs', calories: 300}
-    ],
-    currentItem: null,
-    totalCalories: 0
+  ],
+  currentItem:null,
+  totalCalories:0
+}
+ // public methods
+return {
+  getItems:function(){
+    return data.items
+  },
+  logData:function(){
+    return data;
   }
-
-  // Public methods
-  return {
-    logData: function(){
-      return data;
-    }
-  }
+}
 })();
 
+// ******************** UI  Controller ********************
 
-
-// UI Controller
 const UICtrl = (function(){
-  
-  // Public methods
+UISelectors = {
+  itemList: '#item-list',
+}
+  // public methods
   return {
-
+    populateItems:function(items){
+      let html = '';
+      items.forEach(function(item){
+        html += `<li class="collection-item" id="item-${item.id}">
+        <strong>${item.name}: </strong> <em>${item.calories} : Calories</em>
+        <a href="#" class="secondary-content">
+          <i class="edit-item fa fa-pencil"></i>
+        </a>
+      </li>`
+      });
+      // insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    } 
   }
 })();
 
+//  APP Controller...basically initializes the project
+const AppCtrl = (function(itemCtrl,UICtrl){
 
-
-// App Controller
-const App = (function(ItemCtrl, UICtrl){
-
-  // Public methods
-  return {
-    init: function(){
-      console.log('Initializing App...');
-    }
-  }
+   // public methods
+  return{
+    init:function(){
+      // fetch items from data structure
+      const items = itemCtrl.getItems();
   
-})(ItemCtrl, UICtrl);
+    // populate list with items
+    UICtrl.populateItems(items);
+    },
+  }
+})(itemCtrl,UICtrl);
 
-// Initialize App
-App.init();
+AppCtrl.init();
